e3168c029659f06b723d5a20c5a622144aab3e38
RobsonGap <robsonmie@hotmail.com>
2019-01-27 02:43:46 -0200

    modificando js testes com imagens

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
 var myHeading = document.querySelector('h1');
 myHeading.innerHTML = 'Hello world!';
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
